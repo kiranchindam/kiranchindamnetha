9d9778012e6bf8a79cb6a7c415a9aa5a8cc21804
kiranchindam <kiranchindam432@gmail.com>
2021-09-23 12:19:47 +0530

    Create Arrat.c

---
+++ Arrat.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,n;
+printf("enter array size");
+scanf("%d",&n);
+printf("enter elements into array");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("elements in array are:");
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
+}
