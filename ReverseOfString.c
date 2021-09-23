e652b949fbb0da07f8fe6abaa86ca38514b195be
kiranchindam <kiranchindam432@gmail.com>
2021-09-23 16:51:56 +0530

    Create ReverseOfString.c

---
+++ ReverseOfString.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+  char str[20];
+  int i=0,j;
+  printf("enter a string");
+  gets(str);
+  while(str[i]!='\0')
+  {
+    i++;
+  }
+  int len=i;
+  i--;
+  printf("entered string is :%s",str);
+  for(j=0;j<len/2;j++;i--)
+  {
+    char temp;
+    temp=str[i];
+    str[i]=str[j];
+    str[j]=temp;
+  }
+  printf("Reverse of given string is:%s\n",str);
+}
