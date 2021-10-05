fa970b66c41940bf0d69ebfce35183fad1a60575
kiranchindam <kiranchindam432@gmail.com>
2021-10-05 15:54:50 +0530

    Update ReverseOfString.c

--- ReverseOfString.c
+++ ReverseOfString.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 void main()
 {
-  char str[20];
-  int i=0,j=0;
-  printf("enter a string:");
-  gets(str);
+  char str[20], i;
+  printf("enter a string:\n");
+  gets_s(str);
   while(str[i]!='\0')
   {
     i++;
   }
   int len=i;
   i--;
-  printf("entered string is :%s",str);
-  for(j=0;j<len/2;j++;i--)
+  printf("entered string is:%s\n",str);
+  for(j=0;j<len/2;j++,i--)
   {
     char temp;
     temp=str[i];
     str[i]=str[j];
     str[j]=temp;
   }
-  printf("Reverse of given string is:%s\n",str);
+  printf("Reverse of given string is:%s\n", str);
 }
