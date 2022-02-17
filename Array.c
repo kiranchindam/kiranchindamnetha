d57303b451d8e121b577b2480e653a0a11156d3d
Brand Traversy <kiranchindam432@gmail.com>
2022-02-17 21:38:53 +0530

    rajendra

--- Array.c
+++ Array.c
@@ -5,6 +5,8 @@ void main()
   printf("enter the array size:");
   scanf("%d",&n);
   printf("enter elements into array");
+	printf("enter elements into rajendra array");
+
   for(i=0;i<n;i++)
   {
     scanf("%d",&a[i]);
